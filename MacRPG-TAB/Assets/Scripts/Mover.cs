ff680d8a5c538509de31e5adb02157d9b7025720
paj <pajsaraf@gmail.com>
2023-02-27 20:00:34 -0500

    set up navigation by navmesh
    basic raycast to find where mouse is pressed and then move player to that location

--- MacRPG-TAB/Assets/Scripts/Mover.cs
+++ MacRPG-TAB/Assets/Scripts/Mover.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+[SerializeField] Transform target;
+
+
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {       
+            MoveToCurser();
+        }
     }
+    private void MoveToCurser()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point; 
+            
+        }
+    }
+        
+
 }
+
+
+
