2a44ae9d77c245c79f13cca4f50ecc30b82af0b3
paj <pajsaraf@gmail.com>
2023-03-12 21:03:02 -0400

    added a weapon range and move to target weapon range

--- MacRPG-TAB/Assets/Scripts/Combat/Fighter.cs
+++ MacRPG-TAB/Assets/Scripts/Combat/Fighter.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Movement;
 
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
-        public void Attack(CombatTarget target)
+
+        [SerializeField] float WeaponRange = 2f;
+        Transform target;
+
+
+
+        private void Update()
         {
-            print("Attacking");
+            if (target == null) return;
+            
+            if (target != null && !GetisInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Stop();
+            }
         }
 
+        private bool GetisInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < WeaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+            {
+                target = combatTarget.transform; //fighter knows if it should target because it has a target
+            }
+
+            public void Cancel()
+            {
+                target = null;
+            }
     }
-
-
 }
 
